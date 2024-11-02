51329cc3894701fb5c2b08f41ce0b23976fa6251
Vinicius Benfica <vinicius.benfica.ruy@gmail.com>
2024-11-02 16:23:46 -0300

    ♻️ refactor find user factory

--- src/usecase/user/find/find.user.usecase.factory.ts
+++ src/usecase/user/find/find.user.usecase.factory.ts
@@ -1,8 +1,8 @@
-import UserRepository from '../../../infrastructure/user/repositories/user.repository';
-import FindUserUseCase from './find.user.usecase';
+import UserRepository from "../../../infrastructure/user/repositories/user.repository";
+import FindUserUseCase from "./find.user.usecase";
 
 export default class FindUserUsecaseFactory {
-  static create() {
-    return new FindUserUseCase(new UserRepository());
-  }
+	static create() {
+		return new FindUserUseCase(new UserRepository());
+	}
 }
