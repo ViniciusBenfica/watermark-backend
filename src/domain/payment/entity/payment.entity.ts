a0db7d225f10664a332639dc31f1b732d29ab2bd
Vinicius Benfica <vinicius.benfica.ruy@gmail.com>
2025-02-23 12:07:32 -0300

    ♻️ Remove expirationDate from Payment entity and factory

--- src/domain/payment/entity/payment.entity.ts
+++ src/domain/payment/entity/payment.entity.ts
@@ -1,14 +1,12 @@
 export default class Payment {
 	private _id: string;
 	private _buyDate: Date;
-	private _expirationDate: Date;
 	private _userId: string;
 	private _planId: string;
 
-	constructor(id: string, userId: string, planId: string, buyDate: Date, expirationDate: Date) {
+	constructor(id: string, userId: string, planId: string, buyDate: Date) {
 		this._id = id;
 		this._buyDate = buyDate;
-		this._expirationDate = expirationDate;
 		this._userId = userId;
 		this._planId = planId;
 	}
@@ -19,10 +17,6 @@ export default class Payment {
 
 	get buyDate(): Date {
 		return this._buyDate;
-	}
-
-	get expirationDate(): Date {
-		return this._expirationDate;
 	}
 
 	get userId(): string {
--- src/domain/payment/factory/payment.factory.ts
+++ src/domain/payment/factory/payment.factory.ts
@@ -2,7 +2,7 @@ import { v4 as uuid } from "uuid";
 import Payment from "../entity/payment.entity";
 
 export default class PaymentFactory {
-	static create(userId: string, planId: string, buyDate: Date, expirationDate: Date, id?: string): Payment {
-		return new Payment(id || uuid(), userId, planId, buyDate, expirationDate);
+	static create(userId: string, planId: string, buyDate: Date, id?: string): Payment {
+		return new Payment(id || uuid(), userId, planId, buyDate);
 	}
 }
