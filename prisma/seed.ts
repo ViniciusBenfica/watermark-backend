b08152bc8ff527f9dd8e0acf75e614af3aa5c350
Vinicius Benfica <vinicius.benfica.ruy@gmail.com>
2025-02-25 20:35:04 -0300

    🌱 Update seed data with Stripe product IDs and adjusted plan pricing

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -5,31 +5,31 @@ import { v4 as uuid } from "uuid";
 async function main() {
 	await prisma.plan.create({
 		data: {
-			id: uuid(),
+			id: "prod_RpoY14xLtgK47D",
 			name: "Mensal",
 			description: "Plan description here",
-			price: 250,
+			price: 1500,
 			duration: 1,
 		},
 	});
 
 	await prisma.plan.create({
 		data: {
-			id: uuid(),
+			id: "prod_RpoZMz2Gn8pj3B",
 			name: "Semestral",
 			description: "Plan description here",
-			price: 130,
-			duration: 1,
+			price: 7500,
+			duration: 6,
 		},
 	});
 
 	await prisma.plan.create({
 		data: {
-			id: uuid(),
+			id: "prod_RpobnxxDMgX9Pa",
 			name: "Anual",
 			description: "Plan description here",
-			price: 230,
-			duration: 1,
+			price: 13000,
+			duration: 12,
 		},
 	});
 }
