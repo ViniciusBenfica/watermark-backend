6097fda870b961ff348e0a264b1fed309cd26029
Vinicius Benfica <vinicius.benfica.ruy@gmail.com>
2025-03-09 21:10:36 -0300

    ♻️ Refactor UserCrypter import and formatting

--- src/domain/user/crypter/user.crypter.ts
+++ src/domain/user/crypter/user.crypter.ts
@@ -1,13 +1,13 @@
-import CrypterInterface from '../../../shared/crypter.interface';
-import bcrypt from 'bcrypt';
+import bcrypt from "bcrypt";
+import type CrypterInterface from "../../../shared/crypter.interface";
 
 export default class UserCrypter implements CrypterInterface {
-  private _saltRounds = 10;
+	private _saltRounds = 10;
 
-  crypter(text: string): string {
-    return bcrypt.hashSync(text, this._saltRounds);
-  }
-  compare(text: string, hash: string): boolean {
-    return bcrypt.compareSync(text, hash);
-  }
+	crypter(text: string): string {
+		return bcrypt.hashSync(text, this._saltRounds);
+	}
+	compare(text: string, hash: string): boolean {
+		return bcrypt.compareSync(text, hash);
+	}
 }
